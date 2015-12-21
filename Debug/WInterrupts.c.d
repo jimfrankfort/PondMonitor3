bb44cc7457e07eaf5a61f23f25af1bcd0d4fcbb1
jimfrankfort <jf_orders1@outlook.com>
2015-12-20 18:16:32 -0800

    pre memory optimization

--- Debug/WInterrupts.c.d
+++
@@ -1,7 +0,0 @@
- \
- C:\Users\Jim\ Frankfort\AppData\Local\V.Micro\Arduino\Builds\PondMonitor3\mega2560\WInterrupts.c.o:  \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\WInterrupts.c \
-  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\/wiring_private.h \
-  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\/Arduino.h \
-  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\/binary.h \
-  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\variants\mega/pins_arduino.h
--- Debug/new.cpp.d
+++
@@ -1,4 +0,0 @@
- \
- C:\Users\Jim\ Frankfort\AppData\Local\V.Micro\Arduino\Builds\PondMonitor3\mega2560\new.cpp.o:  \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\new.cpp \
-  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino/new.h
